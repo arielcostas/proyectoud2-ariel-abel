545d43a2375b64e4390b427be6c62c65b0cffcde
Ariel Costas <a21arielcg@iesteis.es>
2022-11-07 13:08:29 +0100

    Hacer la fecha de actualización la actual
    Closes #6
    
    Closes #2

--- dataset/1-esquema.sql
+++ dataset/1-esquema.sql
@@ -3,13 +3,13 @@ CREATE SCHEMA proyectoud2_ariel_abel;
 USE proyectoud2_ariel_abel;
 
 CREATE TABLE artistas(
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     leadStreams BIGINT NOT NULL,
     feats BIGINT NOT NULL,
     tracks INT NOT NULL,
     oneBillion INT NOT NULL,
     hundredMillion INT NOT NULL,
-    lastUpdated date NOT NULL,
+    lastUpdated date DEFAULT NOW() ON UPDATE NOW(),
     PRIMARY KEY (id)
 );
